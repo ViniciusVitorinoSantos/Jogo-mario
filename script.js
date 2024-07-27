0cc4776530156ef19772774af4c2f328a8e3641a
Vitorino.Vinicius <viniciusvitorinodossantos@gmail.com>
2024-07-26 21:36:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,20 @@ const [mario, pipe, restart, scoreBoard, backgroundMusic] = [".mario", ".pipe", 
 
 let score = 0;
 let pipeSpeed = 1.3; // Velocidade inicial dos canos em segundos
-let pipeAnimation = `pipe ${pipeSpeed}s infinite linear`;
 
-pipe.style.animation = pipeAnimation;
-
+// Função para atualizar o marcador de pontos
 const updateScore = () => {
   score++;
   scoreBoard.textContent = `Score: ${score}`;
-  console.log(`Score: ${score}`); // Log de depuração
 };
 
+// Função para atualizar a velocidade dos canos
 const updatePipeSpeed = () => {
-  pipeSpeed -= 0.1; // Aumenta a velocidade dos canos
-  if (pipeSpeed <= 0.5) { // Limita a velocidade mínima
-    pipeSpeed = 0.5;
-  }
-  pipeAnimation = `pipe ${pipeSpeed}s infinite linear`;
-  pipe.style.animation = pipeAnimation;
-  console.log(`Pipe Speed: ${pipeSpeed}s`); // Log de depuração
+  pipeSpeed = Math.max(0.5, pipeSpeed - 0.05); // Aumenta a velocidade dos canos, limitando a velocidade mínima
+  pipe.style.animation = `pipe ${pipeSpeed}s infinite linear`;
 };
 
+// Função de pular
 const jump = () => {
   if (mario) {
     mario.classList.add("jump");
@@ -36,28 +30,24 @@ const jump = () => {
 // Adiciona eventos para toque, clique e tecla
 document.addEventListener("touchstart", (event) => {
   event.preventDefault(); // Impede o comportamento padrão do toque
-  console.log("Touch detected");
   jump();
 });
 
 document.addEventListener("click", (event) => {
-  console.log("Click detected");
   jump();
 });
 
 document.addEventListener("keydown", (event) => {
   if (event.code === "Space") {
-    console.log("Keydown detected");
     jump();
   }
 });
 
+// Função de loop do jogo
 const loop = setInterval(() => {
   if (pipe && mario) {
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window
-      .getComputedStyle(mario)
-      .bottom.replace("px", "");
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
     if (pipePosition <= 100 && pipePosition > 0 && marioPosition < 80) {
       pipe.style.animation = "none";
